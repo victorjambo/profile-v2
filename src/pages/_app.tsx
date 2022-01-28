20855b1e8430fefcdb8aa59d8c056be532031f66
victorjambo <victorjambo@live.com>
2022-01-29 00:09:10 +0300

    dark, light, and system themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Header from "@/components/header";
+import { ThemeProvider } from "next-themes";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <ThemeProvider defaultTheme="system" attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useTheme } from "next-themes";
 
 const Home: NextPage = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
     <div>
       <Head>
@@ -10,8 +13,34 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="flex justify-center items-center text-3xl align-middle h-screen">
-        Welcome Victor
+      <div className={theme}>
+        <div className="bg-white dark:bg-slate-900 text-slate-900 dark:text-white flex justify-center items-center text-3xl align-middle h-screen font-mono">
+          <div className="flex flex-col">
+            <div>Welcome Victor {theme}</div>
+            <div className="flex flex-row space-x-2">
+              <button
+                className="py-2 px-4 bg-slate-900 text-white rounded-md border"
+                onClick={() => setTheme("dark")}
+              >
+                dark
+              </button>
+
+              <button
+                className="py-2 px-4 bg-white text-slate-900 rounded-md border"
+                onClick={() => setTheme("light")}
+              >
+                light
+              </button>
+
+              <button
+                className="py-2 px-4 bg-green-500 rounded-md border"
+                onClick={() => setTheme("system")}
+              >
+                system
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
