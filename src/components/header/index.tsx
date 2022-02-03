a66b553662c7cc9b32ce306d7a939bd2a6cd9a82
victorjambo <victorjambo@live.com>
2022-02-03 12:30:24 +0300

    seo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import { DEV_NAME, DEV_ROLE } from "@/utils/data";
 import FontLoader from "./fontLoader";
+import SEO from "./seo";
 
 const Header: React.FC = () => {
   return (
     <Head>
       <title>{DEV_NAME}</title>
-      <meta name="description" content={DEV_ROLE} />
       <link rel="icon" href="/favicon.ico" />
+      <SEO />
       <FontLoader />
     </Head>
   );
---
+++ src/components/header/seo.tsx
@@ -0,0 +1,27 @@
+const SEO: React.FC = () => {
+  const image = "/images/social/logoBanner.png";
+
+  const title = "Victor Mutai";
+  const description = "Victor Mutai Software Engineer";
+  const twitter = "victor_jambo";
+
+  return (
+    <>
+      <meta name="description" content={description} />
+      <meta name="keywords" content="victor, mutai, web3, engineer" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:site_name" content={title} />
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:creator" content={twitter} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image0" content={image} />
+      <link rel="manifest" href="/manifest.json" />
+    </>
+  );
+};
+
+export default SEO;
