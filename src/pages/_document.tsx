c75941f68a1ab344e06841cc76fe7ee16bb7d91c
victorjambo <victorjambo@live.com>
2022-02-11 01:43:55 +0300

    analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,12 +10,29 @@ export default class MyDocument extends Document {
             dangerouslySetInnerHTML={{
               __html: `(function(h,o,t,j,a,r){
                           h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                          h._hjSettings={hjid:2821702,hjsv:6};
+                          h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR},hjsv:6};
                           a=o.getElementsByTagName('head')[0];
                           r=o.createElement('script');r.async=1;
                           r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                           a.appendChild(r);
                         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            }}
+          />
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+                });
+              `,
             }}
           />
         </Head>
