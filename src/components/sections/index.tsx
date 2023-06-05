fcbf00abc05aaf7fb219526e93be66427d1782d8
victorjambo <victorjambo@live.com>
2023-06-05 20:30:26 +0300

    mobile fixes

--- src/components/sections/index.tsx
+++ src/components/sections/index.tsx
@@ -1,15 +1,11 @@
-import { useDeviceDetect } from "@/context/deviceDetect";
-import { classNames } from "@/utils/classNames";
-
 interface IProps {
   idx: string;
   title: string;
 }
 
 export const SectionHeader: React.FC<IProps> = ({ idx, title }) => {
-  const { isMobile } = useDeviceDetect();
   return (
-    <div className={classNames("section-header-size flex items-end mt-[10px] mb-10 font-semibold leading-[1.1]", isMobile ? "" : "section-header")}>
+    <div className="section-header-size flex items-end mt-[10px] mb-10 font-semibold leading-[1.1] section-header">
       <span className="section-header-size-2 mr-[10px] font-normal relative bottom-1 text-sky-500 dark:text-green">
         {idx}
       </span>
